9c9275a411aebf9c203aa021ecab9fb003ef8a6b
mohammad-rahi <mohammadrahi003@gmail.com>
2023-03-13 22:32:07 +0600

    Added bootstrap setup using webpack sass

---
+++ Frontend/CSS/Frameworks/Bootstrap/src/js/main.js
@@ -0,0 +1,14 @@
+// Import our custom CSS
+
+import '../scss/styles.scss'
+
+import * as bootstrap from 'bootstrap'
+
+import { Popover, Toast, Tooltip } from 'bootstrap'
+
+import Alert from 'bootstrap/js/dist/alert'
+
+// Import all of Bootstrap's JS
+
+
+// or, specify which plugins you need:
